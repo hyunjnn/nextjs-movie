1ee11a92339a6705ef2bf78b99577990db196a2e
hs-2171098-hyunjinchoi <chju0113@naver.com>
2025-04-01 17:51:27 +0900

    content: 에러 페이지 안내 문구를 영어로 변경

--- app/(movies)/movies/[id]/error.tsx
+++ app/(movies)/movies/[id]/error.tsx
@@ -5,23 +5,23 @@ import Link from 'next/link';
 export default function Error({ error, reset }) {
   return (
     <div className="flex flex-col items-center justify-center mt-32 px-4 text-center">
-      <h1 className="text-2xl font-bold">오류가 발생했어요!</h1>
+      <h1 className="text-2xl font-bold text-white">Something went wrong!</h1>
       <p className="text-gray-400 mt-4 mb-12">
-        영화를 불러오는 중 문제가 발생했습니다. <br />
-        네트워크 상태를 확인하거나 잠시 후 다시 시도해주세요.
+        There was a problem loading the movie data. <br />
+        Please check your network connection or try again later.
       </p>
       <div className="flex flex-col gap-3">
         <button
           onClick={() => reset()}
-          className="bg-black text-white px-6 py-2 rounded-full hover:bg-stone-800 transition"
+          className="bg-gray-900 text-white px-6 py-2 rounded-full hover:bg-gray-800 transition"
         >
-          다시 시도하기
+          Try Again
         </button>
         <Link
           href="/"
-          className="bg-white border border-black px-6 py-2 rounded-full text-black hover:bg-gray-100 transition"
+          className="bg-gradient-to-r from-blue-700 via-purple-700 to-orange-700 border border-black px-6 py-2 rounded-full text-black hover:bg-gray-100 transition"
         >
-          ➝ 홈페이지로 이동
+          ➝ Go to Homepage
         </Link>
       </div>
     </div>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,21 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "NotFound",
-  robots: "noindex",
+  title: 'NotFound',
+  robots: 'noindex',
 };
 
 export default function NotFound() {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-32">
-        <h1 className="text-3xl font-bold">404 ERROR</h1>
-        <p className="body1 text-gray-400 mt-4 mb-10">
-          해당 페이지를 찾을 수 없습니다.
-        </p>
+        <h1 className="text-3xl font-bold text-white">404 Error Page</h1>
+        <p className="text-gray-400 mt-4 mb-10">The page you're looking for doesn't exist.</p>
         <a
           href="/"
-          className="flex items-center justify-center rounded-full bg-black text-white h-12 px-8"
+          className="flex items-center justify-center rounded-full font-bold bg-gradient-to-r from-blue-700 via-purple-700 to-orange-700 h-12 px-8"
         >
-          <span>홈페이지로 이동</span>
+          <span className="text-lg">Go to Homepage</span>
         </a>
       </div>
     </>
